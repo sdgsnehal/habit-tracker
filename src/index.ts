febf2f0127abb136eaee56118af7c19a2f51abbf
sdgsnehal <sdgsnehal@gmail.com>
2026-02-22 11:22:51 +0530

    chore: add Prisma and PostgreSQL dependencies to package.json feat: configure CORS with origin from environment variable in index.ts feat: add dotenv and cors imports in index.ts for environment configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+
 const app = express();
 const port = 3000;
+
+app.use(cors({ origin: process.env.CORS_ORIGIN_FRONTEND }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
