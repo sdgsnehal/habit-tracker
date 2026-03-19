1706781391799414aa6a231b3747f4ad3bf2319b
sdgsnehal <sdgsnehal@gmail.com>
2026-03-19 19:26:25 +0530

    moved generated folder

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,7 @@
-import { PrismaClient } from "../../generated/prisma/client.js";
+import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
-
-const { Pool } = pg;
-
-const connectionString = process.env.DATABASE_URL;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-
-export const prisma = new PrismaClient({ adapter });
+import { PrismaClient } from "../generated/prisma/client.js";
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+export { prisma };
